fe20a9ba5c8372881ba8a4701c3fc962112f36e8
EverMoone <133783085+EverMoone@users.noreply.github.com>
2023-08-28 15:32:51 -0500

    Create Aaron.java

---
+++ Files_from_Angel/Aaron.java
@@ -0,0 +1,28 @@
+public class Aaron {
+	public static void aaron() {
+	for (int a = 0; a <= 20; a++) {
+		System.out.print("'");
+	}
+	System.out.println();
+	for (int b = 0; b <= 20; b++) {
+		System.out.print("_");
+	}
+	System.out.println();
+	for (int c = 0; c <= 20; c++) {
+		System.out.print("^");
+	}
+	System.out.println();
+	for (int d = 0; d <= 20; d++) {
+		System.out.print("]");
+	}
+	System.out.println();
+	for (int e = 0; e <= 20; e++) {
+		System.out.print("\\");
+	}
+	System.out.println();
+	for (int i = 0; i <= 20; i++) {
+		System.out.print("[");
+	}
+	System.out.println();
+	}
+}
