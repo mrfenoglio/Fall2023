9fb9241f2ab5565ebd6b77dfc8f16e95d7a09047
mrfenoglio <142730367+mrfenoglio@users.noreply.github.com>
2023-11-07 12:42:46 -0600

    Update FourQueens.java
    With a few comments

--- Files_from_Rudy/FourQueens.java
+++ Files_from_Rudy/FourQueens.java
@@ -1,22 +1,23 @@
 import java.io.*;
 import java.util.*;
 public class FourQueens {
-	static int noTimes = 0;
-	 //static List<List<Integer>> bigList = new ArrayList<>();
+	static int noTimes = 0;// this counts the number of times there is a solution
+	 
 	 public static  List<List<String>> solveNQueens(int n) {
-		 List<List<String>> bigList = new ArrayList<>();
-		 char[][] matrix = new char[n][n];
+		 List<List<String>> bigList = new ArrayList<>(); // stores the answer
+		 char[][] matrix = new char[n][n]; // the chessboard
 		 for(int i = 0; i<matrix.length; i++) {
 			  Arrays.fill(matrix[i], '.');
 			 
 		 }
-		
+		//  the is the backtracking algo
 		bt(bigList, matrix, 0);
 		 
 	   
 		 
 		 return bigList;
 	    }
+	// given a matrix, can we put a queen at row r and column c
 	 static boolean canUse(char[][] matrix, int r, int c) {
 		 for(int i = 0; i<matrix.length; i++) {
 			 if(matrix[i][c]=='Q')
@@ -42,7 +43,10 @@ public class FourQueens {
 		 
 		 
 	 }
+
+// this is the key algo 
 	static  void bt(List<List<String>> bigList, char[][] matrix, int row) {
+		// the base case.  If we have looked at all the rows add an answer to bigList
 		 if(row==matrix.length) {
 			 String toAdd = "";
 			 List<String> nl = new ArrayList<>();
@@ -56,10 +60,13 @@ public class FourQueens {
 		noTimes++;
 			 
 		 }
+			 //the recursive step
 		 else { for(int j = 0; j<matrix.length; j++) {
+			 // can we put a queen at row and col j.
 			 if(canUse(matrix, row, j)) {
 				 matrix[row][j]='Q';
 				 bt(bigList, matrix,row+1);
+				 // the back track step
 				 matrix[row][j]='.';
 				 
 			 }
@@ -71,6 +78,7 @@ public class FourQueens {
 	 }
 	 
 	public static void main(String[] args) {
+		// this is designed for multiple dims
 		for(int i =4; i<5; i++) {
 		List<List<String>> l =solveNQueens(i);
 		System.out.println(i +" "+noTimes);
