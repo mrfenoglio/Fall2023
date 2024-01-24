80d62f52bef6a7c4590beb2b802eb1da29302a9e
mrfenoglio <142730367+mrfenoglio@users.noreply.github.com>
2024-01-24 15:13:24 -0600

    Update Rodrigo.java

--- UIL2023/Rodrigo.java
+++ UIL2023/Rodrigo.java
@@ -6,11 +6,14 @@ public class Rodrigo {
 		int ans = -1;
 		int min = Integer.MAX_VALUE;
 		for(int i= 0; i<visited.length; i++) {
-			if(!visited[i] && min>prices[i])
+			if(!visited[i] && min>prices[i]) {
 				ans = i;
 				min = prices[i];
-			 
+			}
 		}
+//	System.out.println("prices  "+Arrays.toString(prices));
+	//	System.out.println("i "+ans);
+		//System.out.println(Arrays.toString(visited));
 		
 		return ans;
 	}
@@ -26,16 +29,16 @@ public class Rodrigo {
 		//queue.add(dal);
 		for(int i = 0; i<prices.length; i++) {
 			int check = lowest(visited, prices);
-			System.out.println(check);
+		
 			if(check==-1)
 				return prices;
 			visited[check]= true;
 			for(int k = 0; k<prices.length; k++) {
-				if(k!=dal && adj[check][k]!=0) {
+				if(k!=dal && adj[check][k]!=0&& !visited[k]) {
 					prices[k]= Math.min(prices[k], prices[check]+adj[check][k]);
 				}
 			}
-			System.out.println(Arrays.toString(prices));
+			//System.out.println(Arrays.toString(prices));
 			
 		
 			
@@ -51,43 +54,50 @@ public class Rodrigo {
 		Scanner scan = new Scanner(new File("rodrigo"));
 				int cities = scan.nextInt();
 				int flights = scan.nextInt();
+				int  index = 0;
 				int budget = scan.nextInt();
+				Map<String, Integer> map = new HashMap<>();
 				for(int i = 0; i<cities; i++) {
 					String name = scan.next();
 					double rating = scan.nextDouble();
 					double price = scan.nextDouble();
 					int att = scan.nextInt();
 					double cul = scan.nextDouble();
-					list.add(new city(name, rating, price, att, cul));
+					map.put(name, index++);
+					list.add(new city(name, rating, price, att, cul, index-1));
 					
 					
 					
 				}
+				map.put("Dallas", index);
 int[][] adj = new int[cities+1][cities+1];
-Map<String, Integer> map = new HashMap<>();
 int n = flights;
-int  index = 0;
 while(n-->0) {
 	String left = scan.next();
 	String right = scan.next();
 	//System.out.println(left+" "+ right);
 	int price = scan.nextInt();
-	if(!map.containsKey(left))
-		map.put(left, index++);
-	if(!map.containsKey(right))
-		map.put(right, index++);
+	
 	//System.out.println(map);
 	
 	adj[map.get(left)][map.get(right)]=price;
 	adj[map.get(right)][map.get(left)]=price;
 	
 }
-for(int[] row: adj)
-	System.out.println(Arrays.toString(row));
+
 // now we need to find prices from Dallas;
 int[] prices = find("Dallas", map, adj);
-System.out.println(map);
-System.out.println(Arrays.toString(prices));
+for(int i = prices.length-1; i>=0; i--) {
+	if(prices[i]>2000)
+		list.remove(i);
+}
+Comparator<city> comp1 = (a,b)->b.att-a.att;
+Comparator<city> comp2 = (a,b)->b.culinary-a.culinary>0?1:b.culinary-a.culinary<0? -1:0;
+Comparator<city> comp3 = (a,b)->b.price-a.price>0?1:b.price-a.price<0? -1:0;
+Comparator<city> comp4 = (a,b)->b.rating-a.rating>0?1:b.rating-a.rating<0? -1:0;
+list.sort(comp1.thenComparing(comp2).thenComparing(comp3).thenComparing(comp4));
+for(city c: list)
+	System.out.println(c.name);
 }
 }
 	class city{
@@ -96,13 +106,15 @@ System.out.println(Arrays.toString(prices));
 		double price;
 		int att;
 		double culinary;
-		public city(String name, double rating, double price, int att, double culinary) {
+		int index;
+		public city(String name, double rating, double price, int att, double culinary, int ind) {
 			super();
 			this.name = name;
 			this.rating = rating;
 			this.price = price;
 			this.att = att;
 			this.culinary = culinary;
+			index = ind;
 		}
 	}
 			
