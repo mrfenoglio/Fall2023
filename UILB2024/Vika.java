8691c3a60a6de0694627650c616147f527a9226d
mrfenoglio <142730367+mrfenoglio@users.noreply.github.com>
2024-02-13 18:07:38 -0600

    Update Vika.java

--- UILB2024/Vika.java
+++ UILB2024/Vika.java
@@ -7,12 +7,14 @@ public static void main(String[] args) throws IOException{
 		while(noSets-->0) {
 			char[] s1 = scan.next().toCharArray();
 			char[] s2 = scan.next().toCharArray();
+			// solved with dynamic programming
 			int[][] arr = new int[s1.length+1][s2.length+1];
 			int max = 0;
 			for(int i = 1; i<arr.length; i++) {
 				
 				for(int j = 1; j<arr[0].length; j++) {
 					//int toAdd = Math.max()
+					// decide whether to use this char, if we use it we add one to arr[i-1][j-1]
 					if(s2[j-1]==s1[i-1])
 						arr[i][j]=Math.max(1+arr[i-1][j-1], Math.max(arr[i-1][j], arr[i][j-1]));
 					
